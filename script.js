4635bf7bfa682fdaa249fc70a20ac41bd074c287
Kartik B H <95575463+kartikbh56@users.noreply.github.com>
2025-07-03 23:54:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-//your JS code here. If required.
-const counterElement = document.getElementById("counter");
-const button = document.getElementById("incrementBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const counterElement = document.getElementById("counter");
+  const button = document.getElementById("incrementBtn");
 
-button.addEventListener("click", () => {
-  const currentValue = parseInt(counterElement.textContent);
-  alert("Current value before increment: " + currentValue);
-  counterElement.textContent = currentValue + 1;
+  button.addEventListener("click", () => {
+    const currentValue = parseInt(counterElement.textContent);
+    alert("Current value before increment: " + currentValue);
+    counterElement.textContent = currentValue + 1;
+  });
 });
